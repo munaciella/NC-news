e48b5e42048e85e1ed05c539edba655ad784168f
munaciella <franes88@gmail.com>
2023-11-21 16:35:59 +0000

    add tests for section 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -45,23 +45,31 @@ describe('GET /api', () => {
   });
 });
 
-describe.skip('GET /api/articles', () => {
-    test('200: returns an array of articles objects', () => {
-      return request(app)
-        .get('/api/articles')
-        .expect(200)
-        .then(({ body }) => {
-          expect(body.articles).toHaveLength(13);
-          body.articles.forEach((article) => {
-            expect(typeof article.author).toBe('string');
-            expect(typeof article.title).toBe('string');
-            expect(typeof article.article_id).toBe('number');
-            expect(typeof article.topic).toBe('string');
-            expect(typeof article.created_at).toBe('string');
-            expect(typeof article.votes).toBe('number');
-            expect(typeof article.article_img_url).toBe('string');
-            expect(typeof article.comment_count).toBe('number');
-          });
-        })
-    })
-})
+describe('GET /api/articles', () => {
+  test('200: returns an array of articles objects', () => {
+    return request(app)
+      .get('/api/articles')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toHaveLength(5);
+        body.articles.forEach((article) => {
+          expect(typeof article.author).toBe('string');
+          expect(typeof article.title).toBe('string');
+          expect(typeof article.article_id).toBe('number');
+          expect(typeof article.topic).toBe('string');
+          expect(typeof article.created_at).toBe('string');
+          expect(typeof article.votes).toBe('number');
+          expect(typeof article.article_img_url).toBe('string');
+          expect(typeof article.comment_count).toBe('string');
+        });
+      });
+  });
+  test('404: responds with an error message with invalid path', () => {
+    return request(app)
+      .get('/api/article')
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe('path not found');
+      });
+  });
+});
--- models/models.js
+++ models/models.js
@@ -8,9 +8,11 @@ exports.selectApiTopics = () => {
 };
 
 exports.selectApiArticles = () => {
-    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments) AS comment_count FROM articles JOIN comments ON comments.article_id = articles.article_id GROUP BY article_id`)
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments) AS comment_count FROM articles JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC`)
     .then((result) => {
-        console.log(result);
+        if(!result.rows.length) {
+            return Promise.reject({status: 404, msg: 'path not found'})
+        }
         return result.rows
     })
 }
