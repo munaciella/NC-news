7d924a3968a5787d4aa377b742d585f25fea54fd
munaciella <franes88@gmail.com>
2023-11-24 15:33:30 +0000

    add temp test for section 12

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -340,3 +340,23 @@ describe('DELETE /api/comments/:comment_id', () => {
       });
   });
 });
+
+describe.skip('GET /api/articles/:article_id', () => {
+    test('200: responds with an object of article by its id with a comment count', () => {
+      return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then(({ body }) => {
+          body.articles.forEach((article) => {
+            expect(typeof article.author).toBe('string');
+            expect(typeof article.title).toBe('string');
+            expect(typeof article.article_id).toBe('number');
+            expect(typeof article.topic).toBe('string');
+            expect(typeof article.created_at).toBe('string');
+            expect(typeof article.votes).toBe('number');
+            expect(typeof article.article_img_url).toBe('string');
+            expect(typeof article.comment_count).toBe('string');
+          });
+        })
+    })
+})
